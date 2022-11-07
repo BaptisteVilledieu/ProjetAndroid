4d234cc52c28f5c10f49eb6fa27bc4c7b98c39f0
BaptisteVilledieu <baptiste.villedieu2000@gmail.com>
2022-11-07 09:39:19 +0100

    fichier viewmodel et tmdAPI

--- app/src/main/java/fr/baptistevilledieu/monprofil/MainViewModel.kt
+++ app/src/main/java/fr/baptistevilledieu/monprofil/MainViewModel.kt
@@ -1,2 +1,14 @@
 package fr.baptistevilledieu.monprofil
 
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+class MainViewModel {
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.themoviedb.org/3/")
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build();
+
+    val service = retrofit.create(TmdbAPI::class.java)
+}
--- app/src/main/java/fr/baptistevilledieu/monprofil/Model.kt
+++ app/src/main/java/fr/baptistevilledieu/monprofil/Model.kt
@@ -1,2 +1,25 @@
 package fr.baptistevilledieu.monprofil
 
+class TmdbMovieResult(
+    var page: Int=0,
+    val results: List<TmdbMovie> = listOf()
+)
+
+
+class TmdbMovie(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val media_type: String,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)

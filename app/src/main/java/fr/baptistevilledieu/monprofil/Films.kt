d09a3211fe0432c2869bd7d63b5873d8b2f8f3c6
BaptisteVilledieu <baptiste.villedieu2000@gmail.com>
2022-11-15 10:06:41 +0100

    fin de séance, affichage card et début navbar

--- app/src/main/java/fr/baptistevilledieu/monprofil/Films.kt
+++ app/src/main/java/fr/baptistevilledieu/monprofil/Films.kt
@@ -1,20 +1,54 @@
 package fr.baptistevilledieu.monprofil
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.navigation.NavController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
-fun Films(viewModel: MainViewModel){
+fun Films(viewModel: MainViewModel) {
         val movies by viewModel.movies.collectAsState()
 
-        LazyColumn {
+        LazyVerticalGrid(
+                columns = GridCells.Fixed(count=2)
+        ) {
                 items(movies.results) { movie ->
-                        Text(text = movie.title)
+                        Card(modifier = Modifier.padding(12.dp).fillMaxSize(), elevation = 8.dp, shape = RoundedCornerShape(20.dp)) {
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                        val url = "https://image.tmdb.org/t/p/w780/"
+                                        AsyncImage(
+                                                model = url + movie.backdrop_path,
+                                                contentDescription = movie.title
+                                        )
+                                        Text(
+                                                text = movie.title,
+                                                fontWeight = FontWeight.SemiBold,
+                                                textAlign = TextAlign.Center
+                                        )
+                                        Text(
+                                                text = movie.release_date,
+                                                fontStyle = FontStyle.Italic,
+                                                textAlign = TextAlign.Center,
+                                        )
+                                }
+                        }
                 }
         }
 }
--- app/src/main/java/fr/baptistevilledieu/monprofil/MainActivity.kt
+++ app/src/main/java/fr/baptistevilledieu/monprofil/MainActivity.kt
@@ -6,19 +6,20 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material3.windowsizeclass.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Screen
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import fr.baptistevilledieu.monprofil.ui.theme.MonProfilTheme
 import kotlin.math.log
@@ -33,15 +34,30 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
             val model:MainViewModel = viewModel()
             val windowSizeClass = calculateWindowSizeClass(this)
-            NavHost(
-                navController = navController,
-                startDestination = "profil") {
-                composable("profil") {
-                    Profil(windowSizeClass,navController)
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+                            BottomNavigationItem(
+                                icon = { },
+                                label = { Text(text="films")},
+                                selected = currentDestination?.route == "films" == true,
+                                onClick = { }
+                            )
+                        }
                 }
-                composable("films") {
-                    Films(model)
-                }
+            ) { innerPadding ->
+                NavHost(
+                    navController = navController,
+                    startDestination = "profil", modifier = Modifier.padding(innerPadding)) {
+                    composable("profil") {
+                        Profil(windowSizeClass,navController)
+                    }
+                    composable("films") {
+                        Films(model)
+                    }
+            }
             }
         }
     }
